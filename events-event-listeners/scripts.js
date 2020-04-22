eea716e919012d5368811d27ef3143460c08ee41
spaceduck19 <streerd@gmail.com>
2020-04-22 20:37:25 +0200

    updated the events

--- events-event-listeners/scripts.js
+++ events-event-listeners/scripts.js
@@ -6,10 +6,11 @@ $("#second").click(() => {
     $("#first").text("Second one was clicked");
 });
 
-$('#third').click(() => {
-    $("button").css("background", "purple");
+$("#third").click(() => {
+    $("button").css("background", $("button").css("background", $("#color").val()));
   $("button").css("color", "white");
   $("button").css("font-size", "20px");
   $("button").css("border", "0");
   $("button").css("border-radius", "5px");
+  $("#third").attr("disabled", true);
 });
