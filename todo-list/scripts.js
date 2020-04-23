66db8435b998bfca8f5ec4eaa39515517a517931
spaceduck19 <streerd@gmail.com>
2020-04-23 22:46:02 +0200

    updated

--- todo-list/scripts.js
+++ todo-list/scripts.js
@@ -1,35 +1,36 @@
-var inpText=document.getElementById("input");
-    var btnAdd=document.getElementById("add");
-    var container=document.getElementById("container");
+$(function(){
+    $("h1").animate({
+      "margin-left": "100px"
+    }, "slow");
     
-    btnAdd.onclick=function() {
-        let todo = document.getElementById("input").value;
-        const check = document.createElement("input");
-        check.className="todocheck";
-        check.type = "checkbox";
-
-        const button = document.createElement("button");
-        button.className="todobutton";
-        button.innerHTML="x";
+  
+  $('#taskBtn').click(function() {
+    
+      var newTask = $('#taskInputText').val();
+  
+      if(newTask !== '') {
         
-        let span=document.createElement("span");
-        span.className="todocap";
-        span.innerHTML=todo;
+          var count = $("#taskListSection").children().length;
+          
+          $('#taskListSection').append('<li class="list-group-item deletetask bg-success">' + count + '. '+ newTask + '</li>');
+         
+          $('#taskInputText').val('');
+          
+          deleteTasks();
+          
+          setTimeout(function(){
+            $('#taskListSection li.bg-success').removeClass('bg-success');
+          },1000); 
         
-        let div=document.createElement("div");
-        div.className="todo";
-        div.appendChild(span);
-        div.appendChild(check);
-        div.appendChild(button);
-        
-        check.onclick=function() {
-            this.parentNode.style=this.checked?'color:#c0c0c0; text-decoration: line-through;':'color:black';
-        };        
-        button.onclick=function() {
-            this.parentNode.parentNode.removeChild(this.parentNode);
-        };        
-        
-        container.appendChild(div);
-        inpText.value="";
-        inpText.focus();
-    }
+      } else {
+          alert('Come on, you\'re better than that');   
+      }
+  });  
+  });
+  
+  function deleteTasks(){
+   $('.deletetask').click(function(){
+      $(this).remove();
+  }); 
+  
+  }
